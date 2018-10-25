2504a8f001c91c4a45dba102c73668724623a782
caldasdeoliveira <diogocaldasdeoliveira@gmail.com>
2018-10-25 14:44:56 +0100

    added throtle,strafing and gitignore

--- fun.m
+++ fun.m
@@ -3,10 +3,70 @@ function fun(src,event)
 %   Detailed explanation goes here
     
     global loopFlag;
+    global speed;
+    global Angle1;
+    global Angle2;
     disp(event.Key);
     
     if event.Key == 'q'
+        CASK_SendRequest('Speed1',0,'Speed2',0);
         loopFlag = false;
+        
+    elseif strcmp(event.Key,'space')
+        speed=0
+        Angle1=0
+        Angle2=0
+        CASK_SendRequest('Speed1',speed,'Speed2',speed);
+        CASK_SendRequest('Angle1', Angle1, 'Angle2', Angle2);
+  
+    elseif event.Key == 'w'
+        if speed==0
+            speed=50
+        elseif speed==-50
+            speed=0
+        elseif speed<130
+            speed=speed+10
+        end
+        CASK_SendRequest('Speed1',speed,'Speed2',speed);
+
+        
+    elseif event.Key == 's'        
+        if speed==0
+            speed=-50
+        elseif speed==50
+            speed=0
+        elseif speed>-130
+            speed=speed-10
+        end
+        CASK_SendRequest('Speed1',speed,'Speed2',speed);
+
+        
+    elseif event.Key == 'a'
+        if Angle1==0 && Angle2==0;
+            Angle1=270
+            Angle2=270
+        elseif Angle1==90 && Angle2==90;
+            Angle1=0
+            Angle2=0
+        end
+        CASK_SendRequest('Angle1', Angle1, 'Angle2', Angle2);
+    
+    elseif event.Key == 'd'
+        if Angle1==0 && Angle2==0;
+            Angle1=90
+            Angle2=90
+        elseif Angle1==270 && Angle2==270;
+            Angle1=0
+            Angle2=0
+        end
+        CASK_SendRequest('Angle1', Angle1, 'Angle2', Angle2);
+        
+        
+    %elseif event.Key == 'q'
+    %   Angle1=wrapTo360(
+
+    %    CASK_SendRequest('Angle1', Angle1, 'Angle2', Angle2);
+
     end
 end
 
--- joystick2.m
+++ joystick2.m
@@ -1,13 +1,23 @@
+disp('antes init')
 CASK_Init();
+disp('depois init')
 
 CASK_SendRequest('Angle1', 0, 'Angle2', 0);
 
 
-h_fig = figure
+h_fig = figure;
 set(h_fig, 'KeyPressFcn', @fun);
 
 global loopFlag;
 loopFlag = true;
+
+global speed;
+speed=0;
+global Angle1;
+global Angle2;
+Angle1=0;
+Angle2=0;
+
 
 disp('antes ciclo')
 while loopFlag
@@ -18,5 +28,5 @@ while loopFlag
 end
 disp('saiu ciclo')
 
-
+close all;
 CASK_CloseComm();
