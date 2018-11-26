e0fc2c0893a621beb5e9fbb0bb7af2d7a79f2330
caldasdeoliveira <diogocaldasdeoliveira@gmail.com>
2018-11-26 16:48:17 +0000

    add integration to movement model, also change output for easier covariance update later on

--- code/movement_model.m
+++ code/movement_model.m
@@ -1,6 +1,5 @@
-function [robotPoseUpdated,jrp_r,jrp_n] = movement_model(robotPose, controlSignal, noise, wheeldistance)
-%  Model of the movement of the robot for slam. The controlSignal is assumed to be
-%      treated, i.e. is assumed to be [theta_f, V_f*(t-tod), theta_r, V_r*(t-tod)].
+function [robotPoseUpdated, G_t, R_t] = movement_model(robotPose, controlSignal, noise, odomTimeStamp, calcTimeStamp, wheeldistance, nLandmarks, jrp_n)
+%  Model of the movement of the robot for slam.
 %
 %  In:
 %      robotPose= [x ; y ; alpha]
@@ -11,6 +10,7 @@ function [robotPoseUpdated,jrp_r,jrp_n] = movement_model(robotPose, controlSigna
 %      robotPoseUpdated: updated robot pose
 %      jrp_r: Jacobian d(ro) / d(r) - derivate to robotpose
 %      jrp_n: Jacobian d(ro) / d(n) - derivated to noise
+  controlSignal=(calcTimeStamp-odomTimeStamp)*controlSignal;
 
   displacement=controlSignal+noise;
 
@@ -28,25 +28,32 @@ function [robotPoseUpdated,jrp_r,jrp_n] = movement_model(robotPose, controlSigna
 
 
   % Calculate Jacobians
-    jrp_r = [ 1, 0, (-displacement(2)*sind(robotPose(3)+displacement(1)) ...
+     jrp_p=[ 0, 0, (-displacement(2)*sind(robotPose(3)+displacement(1)) ...
                 -displacement(4)*sind(robotPose(3)+displacement(3)))/2;
-        0, 1, (displacement(2)*cosd(robotPose(3)+displacement(1)) ...
+        0, 0, (displacement(2)*cosd(robotPose(3)+displacement(1)) ...
                 +displacement(4)*cosd(robotPose(3)+displacement(3)))/2;
-        0, 0, 1];
+        0, 0, 0];
 
-    jrp_n=[
-        (-displacement(2)*sind(robotPose(3)+displacement(1)))/2,
-        cosd(robotPose(3)+displacement(1))/2,
-        (-displacement(4)*sind(robotPose(3)+displacement(3)))/2,
+%{    jrp_n=[
+        (-displacement(2)*sind(robotPose(3)+displacement(1)))/2, ...
+        cosd(robotPose(3)+displacement(1))/2, ...
+        (-displacement(4)*sind(robotPose(3)+displacement(3)))/2, ...
         cosd(robotPose(3)+displacement(3))/2;
 
-        (displacement(2)*cosd(robotPose(3)+displacement(1)))/2,
-        sind(robotPose(3)+displacement(1))/2,
-        (displacement(4)*cosd(robotPose(3)+displacement(3)))/2,
+        (displacement(2)*cosd(robotPose(3)+displacement(1)))/2, ...
+        sind(robotPose(3)+displacement(1))/2, ...
+        (displacement(4)*cosd(robotPose(3)+displacement(3)))/2, ...
         sind(robotPose(3)+displacement(3))/2;
 
-        displacement(2)*cosd(displacement(1))/wheeldistance,
-        sind(displacement(1))/wheeldistance,
-        -displacement(4)*cosd(displacement(3))/wheeldistance,
+        displacement(2)*cosd(displacement(1))/wheeldistance, ...
+        sind(displacement(1))/wheeldistance, ...
+        -displacement(4)*cosd(displacement(3))/wheeldistance, ...
         -sind(displacement(3))/wheeldistance
     ];
+  %}
+
+
+  %Massage jacobians for Covariance matrix update
+  F=[eye(3), zeros(3,2*nLandmarks)];
+  G_t=eye(3+2*nLandmarks)+F'*jrp_p*F;
+  R_t=F'*jrp_n*F;
