514f9948ea406fcd42383de97723f42bd61e39db
Mrrvm <mrrvm@hotmail.com>
2018-12-06 03:14:02 +0000

    Added landmakr plot

--- code/data/build_real_data.m
+++ code/data/build_real_data.m
@@ -1,5 +1,6 @@
-iterdata = load('/home/imarcher/Dropbox/Tecnico/SA/code/data/ITERdata/iterdata.mat');
-matObj = matfile('/home/imarcher/Dropbox/Tecnico/SA/code/data/ITERdata/iterdata.mat');
+load('/home/imarcher/Dropbox/Tecnico/SA/code/data/ITERdata/bags/iterdata1.mat');
+iterdata = data; clear data;
+matObj = matfile('/home/imarcher/Dropbox/Tecnico/SA/code/data/ITERdata/bags/iterdata1.mat');
 details = whos(matObj);
 aux = details.size;
 iterdataSize = aux(1);
@@ -24,7 +25,7 @@ while true
 
   c = 3;
   for j = 1:cam(i).landmarksSeen
-      cam(i).landmark(j, :) = [str2double(brokenline(c)) 180*str2double(brokenline(c+1))/pi str2double(brokenline(c+2))];
+      cam(i).landmark(j, :) = [str2double(brokenline(c)) str2double(brokenline(c+1)) str2double(brokenline(c+2))];
       c = c+3;  
   end
   
@@ -38,9 +39,25 @@ i = 1;
 k = 1;
 j = 1;
 while j <= cameradataSize 
-    tIter = iterdata.timestamp(i);  
-    tCam = cam(j).timestamp;
-    if tCam <= tIter
+    if i <= iterdataSize
+        tIter = iterdata(i).timestamp;  
+        tCam = cam(j).timestamp;
+        if tCam <= tIter
+            data(k).time = tCam;
+            data(k).option = 1;
+            data(k).landmarksSeen = cam(j).landmarksSeen;
+            data(k).landmark = cam(j).landmark;
+            k = k + 1;
+            j = j + 1;
+        end
+        if tCam > tIter 
+            data(k).time = tIter;
+            data(k).option = 0;
+            data(k).odom(:) = iterdata(i).odom;
+            k = k + 1;
+            i = i + 1;
+        end
+    else
         data(k).time = tCam;
         data(k).option = 1;
         data(k).landmarksSeen = cam(j).landmarksSeen;
@@ -48,20 +65,23 @@ while j <= cameradataSize
         k = k + 1;
         j = j + 1;
     end
-    if tCam > tIter 
-        data(k).time = tIter;
-        data(k).option = 0;
-        data(k).odom(:) = iterdata.odom(i,:);
-        k = k + 1;
-        i = i + 1;
-    end
 end
 
 while i <= iterdataSize
-    data(k).time = iterdata.timestamp(i);
+    data(k).time = iterdata(i).timestamp;
     data(k).option = 0;
-    data(k).odom(:) = iterdata.odom(i, :);
+    data(k).odom(:) = iterdata(i).odom;
     k = k + 1;
     i = i + 1;
 end
+
+k = 1;
+while k <= size(data,2)
+    if data(k).option == 0
+        data(k).odom(2) = pi*data(k).odom(2)/180;
+        data(k).odom(4) = pi*data(k).odom(4)/180;
+    end
+    k = k + 1;
+end
+
 
